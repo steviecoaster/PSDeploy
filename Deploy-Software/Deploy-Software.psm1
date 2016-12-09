a7c904996a1631e29e4d324856924c1c8466829b
Stephen Valdinger <svalding@kent.edu>
2016-12-09 12:02:38 -0500

    Changed the way I report deployments. Used SQL instead of scraping local client logs

--- Deploy-Software/Deploy-Software.psm1
+++ Deploy-Software/Deploy-Software.psm1
@@ -7,9 +7,13 @@
 		
 		[parameter(Mandatory = $false)]
 		[switch]$Inventory,
-		
-		[parameter(Mandatory = $true)]
-		[string]$server
+        
+        [parameter(Mandatory = $true)]
+        [string]$Server,
+               
+        [parameter(Mandatory = $false)]
+        [switch]$Collection
+   	
 		
 		
 	)
@@ -18,41 +22,86 @@
 #Enable verbose output
 $VerbosePreference = "Continue"
 	
-	If ($Deploy)
-	{
-		#Grab the list of packages from the server into an array
-		$PDQPackages = Invoke-Command -ComputerName $server { "SELECT Name from Packages;" | sqlite3.exe "C:\ProgramData\Admin Arsenal\PDQ Deploy\Database.db" } | Sort-Object
+	If ($Deploy){
+		#Grab the list of packages from the server into an array. We can't do Auto-Deploy packages unfortunately, so I'm stripping out any packages with that flag set.
+		$PDQPackages = Invoke-Command -ComputerName $Server { "SELECT Packages.Name from Packages WHERE Packages.IsAutoDeploy IS NULL;" | sqlite3.exe "C:\ProgramData\Admin Arsenal\PDQ Deploy\Database.db" } | Sort-Object
 		#Empty placeholder
 		$targets = ""
 		#Empty array for the custom objects of Packages so I can show ID PackageName to the user.
 		$Packages = @()
-		#Starting ID number, since I don't care to pull it in with the query, this is easier.
-		$i = 1
+        #Emtpy array for the custom objects of Collections so I can show ID CollectionName to the user.
+        $Collections = @()
+		#Starting Package ID number, since I don't care to pull it in with the query, this is easier.
+		$pi = 1
+        #Starting Collection ID number, since I don't care to pull it in with the query, this is easier.
+        $ci = 1
 		#We need an array of items for deployment results. There has to be a more elegant way to do this, but I can't think of anything just yet.
 		$deploytargets = @()
+
+      If($Collection){
+
+        #Pull the names of the collections from the database.
+        $listofcollections = Invoke-Command -ComputerName $Server -ScriptBlock { "SELECT Name from Collections;" | sqlite3.exe "C:\ProgramData\Admin Arsenal\PDQ Inventory\Database.db" } | Sort-Object
+                Foreach ($l in $listofcollections){
+			
+			        $CollectionObject = New-Object System.Object
+			        $CollectionObject | Add-Member -Type NoteProperty -Name ID -Value $ci
+			        $CollectionObject | Add-Member -Type NoteProperty -Name Name -Value $l
+			
+			        $Collections += $CollectionObject
+			        #Increment the ID by 1
+			        $ci++
+		    }
+        
+        #Show the ID and Collection name to the user for selection
+        $Collections | Format-Table
+
+        #Ask user which collection to Deploy too.
+        $cchoice = Read-Host "Enter Collection Name"
+        
+        #Store the ID of the Collection they want to use
+        $selectedcollection = $Collections | Where ID -EQ "$cchoice"
+
+        #Grab the names of the computers in the selected collection. This will be an array
+        $collectiontargets = Invoke-Command -ComputerName $Server -ScriptBlock {pdqinventory GetCollectionComputers $args[0] } -ArgumentList $selectedcollection.Name
+
+        Foreach ($ct in $collectiontargets)
+		{
+			
+			$deploytargets += $ct
+			#Use a here-string to wrap quotes around each item, and add it to $target
+			$targets += @" 
+"$ct" 
+"@
+			
+		}
 		
+		$targets = $targets.Substring(0, $targets.Length - 1)
+}		
 		#Loop through every package from the SQL Query and make a PSCustomObject and put it in the Packages Array.
 		Foreach ($p in $PDQPackages)
 		{
 			
 			$PackageObject = New-Object System.Object
-			$PackageObject | Add-Member -Type NoteProperty -Name ID -Value $i
+			$PackageObject | Add-Member -Type NoteProperty -Name ID -Value $pi
 			$PackageObject | Add-Member -Type NoteProperty -Name Name -Value $p
 			
 			$Packages += $PackageObject
 			#Increment the ID by 1
-			$i++
+			$pi++
 		}
 		
 		#Show the ID and Package to the user for selection
 		$Packages | Format-Table
 		
 		#Store the ID of the package they want to install
-		$choice = Read-Host "Select Package ID"
+		$pchoice = Read-Host "Select Package ID"
 		
 		#Store the package object they want to a variable so I can use it against the PDQ Deploy server for Deployment
-		$selectedpackage = $Packages | Where ID -EQ "$choice"
+		$selectedpackage = $Packages | Where ID -EQ "$pchoice"
 		
+
+     If(!$Collection){
 		#Ask the user what target they want to deploy too.
 		$selectedtargets = (Read-Host "Enter Target Hostname(s) (comma separated)").Split(',')
 		
@@ -69,16 +118,25 @@ $VerbosePreference = "Continue"
 		}
 		
 		$targets = $targets.Substring(0, $targets.Length - 1)
+            
+        }
+       
+        
+        
+        
+        #If the target is a collection, get those machines.
+  
 		
 		#Start the deployment
 		Write-Verbose -Message "Starting Deployment on targets: $targets"
-		Invoke-Command -ComputerName $server -ScriptBlock { pdqdeploy deploy -Package $args[0] -Targets $args[1] } -ArgumentList $selectedpackage.Name, $targets
+		Invoke-Command -ComputerName $Server -ScriptBlock { pdqdeploy deploy -Package $args[0] -Targets $args[1] } -ArgumentList $selectedpackage.Name, $targets
 		
 		#Give the remote processes time to start before we start looking for them. Noone likes a false positive.
-		Start-Sleep -Seconds 10
-		
-		
-		Foreach ($dt in $deploytargets)
+		Start-Sleep -Seconds 45
+#############################################################################################################################################################################		
+	                                                            #SHOW RESULTS#
+#############################################################################################################################################################################	
+Foreach ($dt in $deploytargets)
 		{
 			#Get Deployment Result
 			Do
@@ -90,36 +148,45 @@ $VerbosePreference = "Continue"
 					Out-Null
 				}
 			}
-			Until (!$ProcessesFound)
+			Until (!$ProcessesFound)	
+
+    }
+    
+    Write-Verbose "All deployment processes ended, starting to show results"	
+
+
+$DB = "C:\ProgramData\Admin Arsenal\PDQ Deploy\Database.db"
+$SQL = "Select DeploymentComputers.DeploymentId, DeploymentComputers.ShortName , DeploymentComputerSteps.IsFailed  From DeploymentComputers JOIN DeploymentComputerSteps on DeploymentComputers.DeploymentComputerId = DeploymentComputerSteps.DeploymentComputerId where DeploymentComputers.Deploymentid = (SELECT MAX(DeploymentComputers.Deploymentid) FROM DeploymentComputers);"
+$complete = Invoke-Command -ComputerName $Server -Scriptblock { $SQL | sqlite3.exe $db }
+
+Foreach($c in $complete){
+
+$cname = (($c.Substring($c.IndexOf("|"))).Replace('|',''))
+
+$deployobject = New-Object System.Object
+$deployobject | Add-Member -Type NoteProperty -Name DeploymentID -Value $c.SubString(0,5)
+$deployobject | Add-Member -Type NoteProperty -Name Name -Value $cname.SubString(0, $cname.Length - 1)
+$deployobject | Add-Member -type NoteProperty -Name DeployStatus -Value $c[-1]
+
+#Make the status code human readable.
+If($deployobject.DeployStatus -eq "0"){
+    
+    $return = "Successful!"
+    }
+
+    Else{
+     $return = "Failed!"
+    }
+
+Write-Output "$deployobject.Name deployment $return"
+}
+
+
 			
-			
-			#Grab the log from each target.
-			$find = (Select-String -Path "\\$dt\ADMIN`$\AdminArsenal\PDQDeployRunner\Service.Log" -SimpleMatch "Got exit code:").Line
-			
-			
-			#Grab the exit code, which is the last character of the line we grabbed above.
-			$find = $find.Substring($find.Length - 1)
-			
-			#Display Success/Failure message based on code. 0 is Success, anything else is a failure.
-			If ($find -eq "0")
-			{
-				
-				$deployresult = Write-Output $("$dt" + ':' + ' ' + 'Deployment was successful!')
-				
-			}
-			
-			Else
-			{
-				
-				
-				$deployresult = Write-Output $("$dt" + ":" + ' ' + "Deployment failed! Check console logs")
-				
-			}
-			
-			#Show the user the deployment result. 
-			Write-Output $deployresult
-			
-		}
+}
+#############################################################################################################################################################################
+                                                             #END SHOW RESULTS#
+#############################################################################################################################################################################
 	}
 	
 	elseif ($Inventory)
@@ -130,12 +197,12 @@ $VerbosePreference = "Continue"
 		
 		$UpperTarget = $target.ToUpper()
 		$sql = "Select Applications.Name , Applications.Version FROM Applications WHERE  Applications.Name LIKE '%%$App%%' AND Applications.ComputerId = (SELECT Computers.ComputerId FROM Computers WHERE Computers.Name = '$UpperTarget');"
-		$result = Invoke-Command -ComputerName $server -ScriptBlock { $args[0] | sqlite3.exe 'C:\ProgramData\Admin Arsenal\PDQ Inventory\Database.db' } -ArgumentList $sql
+		$result = Invoke-Command -ComputerName $Server -ScriptBlock { $args[0] | sqlite3.exe 'C:\ProgramData\Admin Arsenal\PDQ Inventory\Database.db' } -ArgumentList $sql
 		
 		Write-Output $result.Replace('|', ' ')
 		
 		
 	}
-}
+
 
 Export-ModuleMember -Function Deploy-Software
